eaeb6e72054282813cccdda69ed791f75ea02057
kavindunavod <knavod898@gmail.com>
2025-06-23 18:56:19 +0530

    create family member UI for signup and add some folders to the existing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserPage from './pages/UserPage';
+import { FamilyMemberReg } from './pages/familemember/signup';
+import { FamilyMemberReg2 } from './pages/familemember/signup-step2';
 
 function App() {
-  return <UserPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<FamilyMemberReg />} />
+        <Route path="/family-member/signup" element={<FamilyMemberReg />} />
+        <Route path="/family-member/signup-step2" element={<FamilyMemberReg2 />} />
+        <Route path="/users" element={<UserPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
