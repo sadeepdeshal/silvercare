856c64cb367e5ac0861a6bd03c5245e409473a3b
kavindunavod <knavod898@gmail.com>
2025-06-27 20:51:15 +0530

    done the signup for doctor

--- server/routes/registerRoutes.js
+++ server/routes/registerRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   createFamilyMemberRegistration,
   createCaregiverRegistration,
+  createDoctorRegistration,
   getRegistrations
 } = require('../controllers/registerController');
 
 router.get('/', getRegistrations);
 router.post('/family-member', createFamilyMemberRegistration);
 router.post('/caregiver', createCaregiverRegistration);
+router.post('/doctor', createDoctorRegistration);
 
 module.exports = router;
