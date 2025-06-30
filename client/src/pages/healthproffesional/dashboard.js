bc634211b1283e43c536f3c358e1d2a2556e0df1
kavindunavod <knavod898@gmail.com>
2025-06-30 11:52:46 +0530

    create the UI's for the sign up for health proffessional

---
+++ client/src/pages/healthproffesional/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HealthDashboard = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <p>This is the health Dashboard - Testing Purpose</p>
+    </div>
+  );
+};
+
+export default  HealthDashboard;
