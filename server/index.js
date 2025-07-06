0280a953a8103ce944f6c6e6ebc90632ad0c9b23
Indipa9 <IndipaPerera1@gmail.com>
2025-07-06 23:57:19 +0530

    Doctor Dashboard created

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ app.use(express.json()); // Important: for parsing JSON body
 
 // Load user routes
 const userRoutes = require('./routes/userRoutes');
-<<<<<<< Updated upstream
+
 app.use('/api/users', userRoutes); // Mount the full route
-=======
+
 const registerRoutes = require('./routes/registerRoutes');
 const authRoutes = require('./routes/authRoutes'); // Add this line
 const elderRoutes = require('./routes/elderRoutes'); // Add this line
@@ -21,10 +21,7 @@ app.use('/api/auth', authRoutes); // Add this line
 app.use('/api/elders', elderRoutes);
 //Indipa Added this line
 app.use('/api/doctor',doctorRoutes);//Indipa added this
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
