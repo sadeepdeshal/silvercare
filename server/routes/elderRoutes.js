3bd4f9500c4cd317e45719a14c3b2c385eaab7ec
Sadeep <sadeepdeshal2@gmail.com>
2025-07-07 00:36:31 +0530

    Build simple Elder dashboard
    Elder dashboard

--- server/routes/elderRoutes.js
+++ server/routes/elderRoutes.js
@@ -8,11 +8,16 @@ const {
   createElder 
 } = require('../controllers/elderController');
 
+const { getElderDetails } = require('../controllers/elder');
+
 // Get all elders for a specific family member
 router.get('/family-member/:familyMemberId', getEldersByFamilyMember);
 
 // Get elder count for a specific family member
 router.get('/count/:familyMemberId', getElderCount);
+
+// Fetch elder details - MUST BE BEFORE /:elderId route
+router.get('/elderDetails', getElderDetails);
 
 // Get specific elder by ID
 router.get('/:elderId', getElderById);
