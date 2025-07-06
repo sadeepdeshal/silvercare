18e4e8eb363b77ff641801628f0a446203328014
Indipa9 <IndipaPerera1@gmail.com>
2025-07-07 01:34:21 +0530

    slight change

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/api/users', userRoutes); // Mount the full route
 app.use('/api/register', registerRoutes);
 app.use('/api/auth', authRoutes); // Add this line
 app.use('/api/elders', elderRoutes);
-app.use('/api/doctors', doctorRoutes); // Indipa Added this line
+app.use('/api/doctor', doctorRoutes); // Indipa Added this line
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
